c14ed83fe0f2e59a9f9c344fa4ac0d622b36e73b
Daison Carino <daison12006013@gmail.com>
2015-02-03 01:24:45 +0100

    removed the sample navigations

--- src/config/navigation.php
+++ src/config/navigation.php
@@ -7,62 +7,6 @@ return [
         'url' => '/admin/dashboard',
     ],
 
-    'tagging' => [
-        'name' => 'Tagging',
-        'icon' => 'fa fa-edit fa-fw',
-        'url' => '#',
-        'items' => [
-
-
-            'receiving' => [
-                'name'      => 'Receiving',
-                'url'       => '/admin/tagging/receiving',
-            ],
-            'submitted' => [
-                'name'      => 'Submitting',
-                'url'       => '/admin/tagging/submitting',
-            ],
-            'processing' => [
-                'name'      => 'Processing',
-                'url'       => '/admin/tagging/processing',
-            ],
-
-            'dispatching' => [
-                'name'      => 'Dispatching',
-                'url'       => '/admin/tagging/dispatching',
-            ],
-            'delivered' => [
-                'name'      => 'Delivered',
-                'url'       => '/admin/tagging/delivered',
-            ],
-
-            'incomplete' => [
-                'name'      => 'Incomplete',
-                'url'       => '/admin/tagging/incomplete',
-            ],
-            'damaged' => [
-                'name'      => 'Damaged',
-                'url'       => '/admin/tagging/damaged',
-            ],
-        ],
-    ],
-
-    'reports' => [
-        'name' => 'Reports',
-        'icon' => 'fa fa-table fa-fw',
-        'url' => '#',
-        'items' => [
-            'report1' => [
-                'name' => 'Import Courier A',
-                'url' => '/admin/report/import-courier-a',
-            ],
-            'report2' => [
-                'name' => 'Import Courier B',
-                'url' => '/admin/report/import-courier-b',
-            ],
-        ],
-    ],
-
 
 
     // ----------------------------------------------------------------
