6ab820854b30d63d02eee8b063c0de9630b6d3cf
Daison Carino <daison12006013@gmail.com>
2015-02-11 01:15:34 +0100

    refactored the admin class

--- src/Daison/Admin/Admin.php
+++ src/Daison/Admin/Admin.php
@@ -9,60 +9,52 @@ class Admin
   /**
    *
    *
-   * @param unknown $routes
+   * @param mixed $routes
+   * @return mixed
    */
   public function start($routes)
   {
     // Create the routes and filters
     foreach ($routes as $route_name => $val) {
-      call_user_func_array(array($this, "setRoute"), [$val]);
+      $this->setRoute($val);
     }
 
     return $this;
   }
 
+
   /**
    *
    *
-   * @param unknown $val
-   * @param unknown $controller
-   * @param unknown $action
-   * @param unknown $p1         (optional)
-   * @param unknown $p2         (optional)
-   * @param unknown $p3         (optional)
-   * @param unknown $p4         (optional)
-   * @param unknown $p5         (optional)
-   * @param unknown $p6         (optional)
-   * @param unknown $p7         (optional)
-   * @param unknown $p8         (optional)
-   * @return unknown
    */
-  private function setRoute($val)
+  protected function setRoute($param)
   {
-    $url = explode('@', $val['uses']);
+    $url = explode('@', $param['uses']);
     $controller = $url[0];
     $action = $url[1];
 
-    \Route::{$val['process']}($val['url'],
-      function($p1=null, $p2=null, $p3=null, $p4=null, $p5=null, $p6=null, $p7=null, $p8=null)
-      use ($val, $controller, $action)
+    \Route::{$param['process']}($param['url'],
+      function($p1=null, $p2=null, $p3=null, $p4=null, $p5=null, $p6=null, $p7=null, $p8=null, $p9=null, $p10=null)
+      use ($param, $controller, $action)
       {
 
-        // always check the auth for each route
-        if (! $this->_authCheck($val)) {
+        // check if the route requires an authentication
+        // redirect the guest to the login page
+        if (! $this->isAuthenticated($param)) {
           return \Redirect::to(\Config::get('admin::routes.admin.url'))->withError(\Config::get('admin::lang/lang.login_notifier'));
         }
 
 
-        // check the access
-        if (\Auth::check() == true && $this->_checkAccessList($val) == false ) {
-          $msg = 'Access not allowed for ' . \Auth::user()->email . ' accessing ' . $val['url'];
+        // check if user attempted to login
+        // check if the user roles if it linked to the route roles.
+        if (\Auth::check() == true && $this->hasAnAccess($param) == false ) {
+          $msg = 'Access not allowed for ' . \Auth::user()->email . ' accessing ' . $param['url'];
           \Log::error($msg);
           return \Response::view('admin::admin.errors.roles', [], 404);
         }
 
 
-        return \App::make($controller)->{$action}($p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8);
+        return \App::make($controller)->{$action}($p1, $p2, $p3, $p4, $p5, $p6, $p7, $p8, $p9, $p10);
       }
     );
   }
@@ -71,16 +63,16 @@ class Admin
   /**
    *
    *
-   * @param unknown $val
-   * @return unknown
+   * @param mixed $param
+   * @return bool
    */
-  private function _checkAccessList($val)
+  public function hasAnAccess($param)
   {
-    if (! isset($val['roles'])) {
+    if (! isset($param['roles'])) {
       return true;
     }
 
-    if (count($val['roles']) <= 0) {
+    if (count($param['roles']) <= 0) {
       return true;
     }
 
@@ -88,13 +80,13 @@ class Admin
     $user->roles;
 
     foreach ($user['roles'] as $role) {
-      if (in_array($role['name'], $val['roles'])) {
+      if (in_array($role['name'], $param['roles'])) {
         return true;
       }
     }
 
     $current_url = \URL::to(\Route::getCurrentRoute()->getPath());
-    if (\URL::to($val['url']) == trim($current_url, '/')) {
+    if (\URL::to($param['url']) == trim($current_url, '/')) {
       return false;
     } else {
       return true;
@@ -105,18 +97,18 @@ class Admin
   /**
    *
    *
-   * @param unknown $val
-   * @return unknown
+   * @param mixed $param
+   * @return bool
    */
-  private function _authCheck($val)
+  public function isAuthenticated($param)
   {
     $current_url = \URL::to(\Route::getCurrentRoute()->getPath());
 
     // check current url and match to the url
-    if (trim($current_url, '/') == \URL::to($val['url'])) {
+    if (trim($current_url, '/') == \URL::to($param['url'])) {
 
       // matched, then check if is_auth is set and if it is true
-      if (isset($val['is_auth']) && $val['is_auth'] == true) {
+      if (isset($param['is_auth']) && $param['is_auth'] == true) {
 
         // set, then we need to check if auth or not, redirect.
         if (\Auth::check() == false) {
