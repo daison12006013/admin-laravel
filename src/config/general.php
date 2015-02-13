5169b5775169b00d6d358788b0b2529bdb93a45a
Daison Carino <daison12006013@gmail.com>
2015-02-13 01:27:14 +0100

    changed the alignment of the general configuration file

--- src/config/general.php
+++ src/config/general.php
@@ -2,34 +2,32 @@
 
 return [
 
-  'language'                      => 'en',
+  'site_name'                     => 'SENS',                                     // Your system name
+  'version'                       => '1.0.0',                                    // Current system version, try to hover your mouse to your system name upon logging in
+  'language'                      => 'en',                                       // Language to use, check config/lang/{en}.php
 
-  'site_name'                     => 'SENS',
-  'version'                       => '1.0.0',
-
-  'email'             => [
-    'from'                        => \Config::get('app.email.from'),
-    'name'                        => \Config::get('app.email.name'),
-  ],
-
-  'password_settings' => [
-    'min'                         => 8,
-    'has_number'                  => false,
-    'has_special_char'            => false,
-    'has_upper_and_lower'         => false,
-
-    'autolock'                    => true,
-    'autolock_attempt'            => 5,
-
-    'reset_prefix'                => '!Pwd',
+  'email'                         => [
+    'from'                        => \Config::get('app.email.from'),             // Email to be used e.g('noreply@domain.com')
+    'name'                        => \Config::get('app.email.name'),             // Your email name e.g('Acme Email System')
   ],
 
 
-  // Advanced Configuration, You must read the documentation @ github
-  'enable_top_nav'                => false,
-  'top_nav_template'              => 'admin::admin.layouts.top_nav_template',
+  /*-------------------------------------------------------------------------
+  | Advanced configuration
+  | -------------------------------------------------------------------------
+   */
+  'enable_top_nav'                => false,                                       // To enable the top navigation
+  'top_nav_template'              => 'admin::admin.layouts.top_nav_template',     // Target template
+  'user_lists_count'              => 30,                                          // The count lists
 
-
-  'user_lists_count'              => 30,
+  'password_settings'             => [
+    'min'                         => 8,                                           // Minimum password to required
+    'has_number'                  => false,                                       // Password should have atleast 1 numeric
+    'has_special_char'            => false,                                       // Password should have atleast 1 special character
+    'has_upper_and_lower'         => false,                                       // Password should have atleast Upper and Lower case character
+    // 'autolock'                    => true,
+    // 'autolock_attempt'            => 5,
+    'reset_prefix'                => '!Pwd',                                      // Resetting of password, starts with, by defaylt "!Pwd"
+  ],
 
 ];
